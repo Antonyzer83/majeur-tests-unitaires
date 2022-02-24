c43e05e7a44fc86a17397c7c7d9063f87a7abe7d
Antony CASTANER <antony.castaner@ynov.com>
2022-02-24 17:09:32 +0100

    Add complete tests

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -11,58 +11,72 @@ class ApiTest extends WebTestCase
         $client = static::createClient();
         // Request a specific page
         $client->jsonRequest('POST', '/api/calculator/addition', [
-                'value1' => '1',
-                'value2' => '1',
-            ]);
+            'value1' => '1',
+            'value2' => '1',
+        ]);
         $response = $client->getResponse();
         $this->assertResponseIsSuccessful();
         $this->assertJson($response->getContent());
         $responseData = json_decode($response->getContent(), true);
         $this->assertEquals(['value' => 2], $responseData);
     }
-	
-	public function testApiSoustraction(): void
+
+    public function testApiSoustraction(): void
     {
         $client = static::createClient();
         // Request a specific page
         $client->jsonRequest('POST', '/api/calculator/substraction', [
-                'value1' => '1',
-                'value2' => '1',
-            ]);
+            'value1' => '1',
+            'value2' => '1',
+        ]);
         $response = $client->getResponse();
         $this->assertResponseIsSuccessful();
         $this->assertJson($response->getContent());
         $responseData = json_decode($response->getContent(), true);
         $this->assertEquals(['value' => 0], $responseData);
     }
-	
-	public function testApiDivision(): void
+
+    public function testApiDivision(): void
     {
         $client = static::createClient();
         // Request a specific page
         $client->jsonRequest('POST', '/api/calculator/division', [
-                'value1' => '9',
-                'value2' => '3',
-            ]);
+            'value1' => '9',
+            'value2' => '3',
+        ]);
         $response = $client->getResponse();
         $this->assertResponseIsSuccessful();
         $this->assertJson($response->getContent());
         $responseData = json_decode($response->getContent(), true);
         $this->assertEquals(['value' => 3], $responseData);
     }
-	
-	public function testApiSquare(): void
+
+    public function testApiModulo(): void
     {
         $client = static::createClient();
         // Request a specific page
-        $client->jsonRequest('POST', '/api/calculator/square', [
-                'value1' => '4',
-                'value2' => '3',
-            ]);
+        $client->jsonRequest('POST', '/api/calculator/modulo', [
+            'value1' => '5',
+            'value2' => '3',
+        ]);
         $response = $client->getResponse();
         $this->assertResponseIsSuccessful();
         $this->assertJson($response->getContent());
         $responseData = json_decode($response->getContent(), true);
-        $this->assertEquals(['value' => 12], $responseData);
+        $this->assertEquals(['value' => 2], $responseData);
+    }
+
+    public function testApiSquare(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('POST', '/api/calculator/square', [
+            'value1' => '9'
+        ]);
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertEquals(['value' => 3], $responseData);
     }
 }

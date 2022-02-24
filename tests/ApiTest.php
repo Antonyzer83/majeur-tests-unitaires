9fabf997685457d0f06eca0ab94e9db8fe8ab014
Antony CASTANER <antony.castaner@ynov.com>
2022-02-24 15:59:48 +0100

    Create unit tests for operations

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -20,4 +20,49 @@ class ApiTest extends WebTestCase
         $responseData = json_decode($response->getContent(), true);
         $this->assertEquals(['value' => 2], $responseData);
     }
+	
+	public function testApiSoustraction(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('POST', '/api/calculator/substraction', [
+                'value1' => '1',
+                'value2' => '1',
+            ]);
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertEquals(['value' => 0], $responseData);
+    }
+	
+	public function testApiDivision(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('POST', '/api/calculator/division', [
+                'value1' => '9',
+                'value2' => '3',
+            ]);
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertEquals(['value' => 3], $responseData);
+    }
+	
+	public function testApiSquare(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('POST', '/api/calculator/square', [
+                'value1' => '4',
+                'value2' => '3',
+            ]);
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertEquals(['value' => 12], $responseData);
+    }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -12,7 +12,52 @@ class CalculatorTest extends KernelTestCase
         self::bootKernel();
         $container = static::getContainer();
         $calculatorService = $container->get(CalculatorService::class);
-        $result  = $calculatorService->addition(1,1);
+        $result  = $calculatorService->addition(1, 1);
         $this->assertEquals(2, $result);
+    }
+
+    public function testSoustraction(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $calculatorService = $container->get(CalculatorService::class);
+        $result  = $calculatorService->substraction(1, 1);
+        $this->assertEquals(0, $result);
+    }
+
+    public function testDivision(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $calculatorService = $container->get(CalculatorService::class);
+        $result  = $calculatorService->division(8, 2);
+        $this->assertEquals(4, $result);
+    }
+
+    public function testMultiplication(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $calculatorService = $container->get(CalculatorService::class);
+        $result  = $calculatorService->square(3, 4);
+        $this->assertEquals(12, $result);
+    }
+
+    public function testModulo(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $calculatorService = $container->get(CalculatorService::class);
+        $result  = $calculatorService->modulo(4, 3);
+        $this->assertEquals(1, $result);
+    }
+
+    public function testRacineCarre(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $calculatorService = $container->get(CalculatorService::class);
+        $result  = $calculatorService->racineCarre(9);
+        $this->assertEquals(3, $result);
     }
 }
